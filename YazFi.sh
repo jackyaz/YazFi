df8f4fa14b658d1aad18ea3e56f298c11ffbc9fa
Jack <jack.naisbett@gmail.com>
2019-03-17 12:12:49 +0000

    Fix Shellcheck error

--- YazFi.sh
+++ YazFi.sh
@@ -1536,7 +1536,7 @@ Menu_GuestConfig(){
 		
 		case "$selectedguest" in
 			1|2|3|4|5|6|7|8|9)
-				selectediface="$(echo "$IFACELIST" | awk '{print $'$selectedguest'}')"
+				selectediface="$(echo "$IFACELIST" | awk '{print $'"$selectedguest"'}')"
 			;;
 			e)
 				exitmenu="true"
