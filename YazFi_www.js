88bc6b93a6bb18ef1df1e341e16518dcd39da6e2
Jack Naisbett <jack.naisbett@gmail.com>
2021-07-18 16:51:50 +0100

    Time conversion functions

--- YazFi_www.js
+++ YazFi_www.js
@@ -647,3 +647,23 @@ $j.fn.serializeObject = function(){
 	});
 	return o;
 };
+
+function StoHHMMSS(secs){
+	var sec_num = parseInt(secs, 10)
+	var hours   = Math.floor(sec_num / 3600)
+	var minutes = Math.floor(sec_num / 60) % 60
+	var seconds = sec_num % 60
+	
+	return [hours,minutes,seconds].map(v => v < 10 ? "0"+v : v).filter((v,i) => v !== "00" || i > 0).join(":");
+}
+
+function HHMMSStoS(HHMMSS){
+	var p = HHMMSS.split(':')
+	var s = 0
+	var m = 1;
+	while (p.length > 0){
+		s += m * parseInt(p.pop(), 10);
+		m *= 60;
+	}
+	return s;
+}
